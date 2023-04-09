deb5a2ace3aa4c6c38c8d1c2598892a48c56c8a7
Michael Weatherby <maw23456789@gmail.com>
2023-04-09 12:33:45 -0400

    Cleanup

--- src/main/java/dev/smithed/radon/mixin/ServerWorldMixin.java
+++ src/main/java/dev/smithed/radon/mixin/ServerWorldMixin.java
@@ -1,5 +1,6 @@
 package dev.smithed.radon.mixin;
 
+import com.mojang.datafixers.DataFixer;
 import dev.smithed.radon.mixin_interface.IEntityIndexExtender;
 import dev.smithed.radon.mixin_interface.IServerWorldExtender;
 import dev.smithed.radon.mixin_interface.ISimpleEntityLookupExtender;
@@ -7,18 +8,24 @@ import dev.smithed.radon.utils.SelectorContainer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonPart;
+import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.SaveLoader;
+import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ApiServices;
 import net.minecraft.util.TypeFilter;
 import net.minecraft.util.function.LazyIterationConsumer;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.entity.EntityIndex;
 import net.minecraft.world.entity.EntityLookup;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -29,7 +36,6 @@ public abstract class ServerWorldMixin implements IServerWorldExtender {
     @Shadow @Final MinecraftServer server;
     @Shadow abstract <T extends Entity> List<? extends T> getEntitiesByType(TypeFilter<Entity, T> filter, Predicate<? super T> predicate);
     @Shadow abstract EntityLookup<Entity> getEntityLookup();
-    @Shadow abstract Profiler getProfiler();
 
     @Override
     public EntityIndex<Entity> getEntityIndex() {
@@ -37,35 +43,6 @@ public abstract class ServerWorldMixin implements IServerWorldExtender {
             return lookup.getIndex();
         else
             return null;
-    }
-
-    @Override
-    public <T extends Entity> void collectEntitiesByType(TypeFilter<Entity, T> filter, Box box, Predicate<? super T> predicate, List<? super T> result, int limit, SelectorContainer container) {
-        this.getProfiler().visit("getEntities");
-        this.getEntityLookup().forEachIntersects(filter, box, (entity) -> {
-            if (predicate.test(entity)) {
-                result.add(entity);
-                if (result.size() >= limit) {
-                    return LazyIterationConsumer.NextIteration.ABORT;
-                }
-            }
-
-            if (entity instanceof EnderDragonEntity enderDragonEntity) {
-                EnderDragonPart[] var6 = enderDragonEntity.getBodyParts();
-
-                for (EnderDragonPart enderDragonPart : var6) {
-                    T entity2 = filter.downcast(enderDragonPart);
-                    if (entity2 != null && predicate.test(entity2)) {
-                        result.add(entity2);
-                        if (result.size() >= limit) {
-                            return LazyIterationConsumer.NextIteration.ABORT;
-                        }
-                    }
-                }
-            }
-
-            return LazyIterationConsumer.NextIteration.CONTINUE;
-        });
     }
 
     @Override
